3e7edda14d5573065ba698c672c52b9027de4669
Meghana Kshirsagar <meghanak@ymail.com>
2016-02-01 10:24:29 -0500

    checkpoint before coordinate desc..

--- sqGroupLasso.m
+++ sqGroupLasso.m
@@ -4,5 +4,5 @@ function [Wnew] = sqGroupLasso(W, Y, X, XX, XY, U, opts)
   opts.tol=1e-6;
 	opts.eta=1e-5;
 	g_idx = U;
-  [Wnew, obj, time] = accgrad(W, Y, X, XX, XY, g_idx, opts);
+  [Wnew, obj, time] = subgradDes(W, Y, X, XX, XY, g_idx, opts);
 end
